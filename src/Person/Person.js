23f423afab2db06d877d75a6d6977964c1cdc27d
Matt Jamison <thirdshiftvideo@gmail.com>
2020-02-13 15:07:13 -0500

    48-adding-styling props.property NOT props.method

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -6,7 +6,7 @@ const person = ( props ) => {
         <div className="Person">
             <p>I'm {props.name} and I am {props.age} years old!</p>
             <p onClick={props.click}>{props.children}</p>
-            <input type="text" onChange={props.nameChangeHandler} value={props.name}/>
+            <input type="text" onChange={props.changed} value={props.name}/>
         </div>
     )
 };
